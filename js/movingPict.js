f31759d7add66657af1cc4fe9e3a81b49f00859a
feriawan125 <ferimario6@gmail.com>
2020-05-09 02:38:40 +0700

    Auto Push

---
+++ js/movingPict.js
@@ -0,0 +1,109 @@
+let file1, file2;
+let img1, img2, img3, img4;
+let w1, w2, weightNum, weightSlider, weightVal;
+let first;
+let sizeX, SizeY
+
+
+function setup(){
+    frameRate(15);
+    sizeX = displayWidth-10;
+    SizeY = displayHeight;
+    first = true;
+    
+    file1 = createFileInput(handleFileInput1);
+    file1.position(15,100);
+    file2 = createFileInput(handleFileInput2);
+    file2.position(300,100);
+    
+    createCanvas(sizeX, SizeY);
+
+    weightNum = select('#pictWeightNum');
+    weightNum.input(startBlend);
+
+    weightSlider = select('#pictWeight');
+    weightSlider.input(startBlend);
+
+    // noLoop();
+}
+function startBlend(){
+    weightVal = this.value()
+    w1 = weightVal/100;
+    w2 = (100 - weightVal)/100;
+    print(w2);
+    blending();
+}
+function firstBlend(){
+    weightVal = 50
+    w1 = weightVal/100;
+    w2 = (100 - weightVal)/100;
+    print(w2);
+    blending();
+    
+}
+function draw() {
+    if (img1) {
+        image(img1, 0, 5, 500, 500);
+        file1.hide();
+    }
+    if (img2) {
+        image(img2, 505, 5, 500, 500);
+        file2.hide();
+    }
+    if (img3 && !first) {
+        image(img3, 252, 505, 500, 500);
+    }
+
+    if (img1 && img2 && first == true) {
+        img3 = img1.get();
+        first = false;
+        firstBlend();
+        if (img3) {
+            image(img3, 252, 505, 500, 500);
+        }
+    }
+}
+
+function handleFileInput1(file){
+    if (file.type === 'image') {
+        img1 = loadImage(file.data, drawPict1);
+        img1.resize(500, 500);
+        draw()
+    }
+}
+function handleFileInput2(file){
+    if (file.type === 'image') {
+        img2 = loadImage(file.data, drawPict2);
+        img2.resize(500, 500);
+        draw();
+    }
+}
+
+function drawPict1(){
+    image(img1, 0, 5, 500, 500);
+    file1.hide();
+}
+function drawPict2(){
+    image(img2, 505, 5, 500, 500);
+    file2.hide();
+}
+
+
+function blending(){
+    clear();
+    if(img1 && img2){
+        img1.loadPixels();
+        img2.loadPixels();
+        img3.loadPixels();
+        for (let y = 0; y < img3.height; y++) {
+            for (let x = 0; x < img3.width; x++) {
+              var index = (x + y * img3.width) * 4;
+      
+              img3.pixels[index] = round(img1.pixels[index]*w1 + img2.pixels[index]*w2);
+              img3.pixels[index+1] = round(img1.pixels[index+1]*w1 + img2.pixels[index+1]*w2);
+              img3.pixels[index+2] = round(img1.pixels[index+2]*w1 + img2.pixels[index+2]*w2);
+            }
+            img3.updatePixels();
+        }
+    }
+}
