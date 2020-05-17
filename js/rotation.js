76f8088e6b06a98a56f0087a7c66ad4a94069033
feriawan125 <ferimario6@gmail.com>
2020-05-17 17:03:59 +0700

    Auto Push

--- js/rotation.js
+++ js/rotation.js
@@ -1,11 +1,13 @@
 let file1;
 let img1;
+let firstTime;
 let degree, degreeVal;
 let sizeX, SizeY
 
 function setup() {
     imageMode(CENTER);
     frameRate(15);
+    firstTime = true;
     sizeX = displayWidth-10;
     SizeY = displayHeight;
     createCanvas(sizeX, SizeY);
@@ -19,15 +21,27 @@ function setup() {
 function draw() {
     if (img1) {
         image(img1, 300, 305, 500, 500);
+        if (!firstTime){
+            img1.remove();
+        }
     }
 }
 function rotatePict() {
     degreeVal = this.value();
     print(degreeVal);
-    translate(width / 2, height / 2);
-    rotate(PI/180*degreeVal);
     clear();
-    draw();
+  if (img1) {
+    image(img1, width / 2 - 125, file1.height + 5, 250, 250);
+    var temp = img1.get();
+    temp.resize(250, 250);
+    
+    push();
+    imageMode(CENTER);
+    translate(width / 2, 500);
+    rotate(PI / 180 * degreeVal);
+    image(temp, 0,0);
+    pop();
+  }
 }
 function handleFileInput1(file){
     if (file.type === 'image') {
