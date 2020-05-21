f688cdfa5f2102dd0daaba17b70f0b33374d3120
feriawan125 <ferimario6@gmail.com>
2020-05-22 02:31:59 +0700

    Auto Push

--- js/smoothing.js
+++ js/smoothing.js
@@ -3,11 +3,31 @@ let img, tempImg;
 let pix;
 let imgR, imgG, imgB;
 let tempR, tempG, tempB;
+let sizeX, sizeY;
+let first;
 
 
 function setup() {
+    first = true;
+    sizeX = displayWidth-10;
+    sizeY = displayHeight;
     
+    fileInput = createFileInput(handleFileInput);
+    fileInput.position(5, 50);
+    
+    createCanvas(sizeX, sizeY);
 }
 function draw() {
-    
+    if(img){
+        image(img, 5, 60, 500, 500);
+    }
+}
+
+function handleFileInput(file) {
+    if (file.type === 'image') {
+        // img = createImage(500, 500);
+        img = loadImage(file.data);
+        img.resize(500, 500);
+        draw();
+    }
 }
